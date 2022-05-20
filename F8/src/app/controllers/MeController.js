dc4be4558af6e76640b5c7a18cdb391dddb3b113
Xuan Long <xuanlong22000@gmail.com>
2022-05-20 16:13:11 +0700

    Lesson22

--- F8/src/app/controllers/MeController.js
+++ F8/src/app/controllers/MeController.js
@@ -2,10 +2,15 @@ const Courses = require('../models/Courses')
 const { multipleMongooseObject } = require('../../util/mongoose')
 
 exports.storedCourses = (req, res, next) => {
-    Courses.find({})
-        .then((course) => {
-            res.render('me/stored-courses', { courses: multipleMongooseObject(course) })
+    Promise.all([Courses.find({}), Courses.countDocumentsDeleted()])
+        .then(([courses, deletedCount]) => {
+            res.render('me/stored-courses', { deletedCount, courses: multipleMongooseObject(courses) })
         })
+
+    // Courses.find({})
+    //     .then((course) => {
+    //         res.render('me/stored-courses', { courses: multipleMongooseObject(course) })
+    //     })
 }
 
 exports.trashCourses = (req, res, next) => {
